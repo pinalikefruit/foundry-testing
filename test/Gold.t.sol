526b2218150d49567a54d047eea7acce1812c2a1
pinalikefruit <pinajoma0@gmail.com>
2023-04-06 09:36:47 -0500

    [INIT] project setup

---
+++ test/Gold.t.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Gold.sol";
+
+contract GoldTest is Test {
+    Gold public gold;
+
+    function setUp() public {}
+}
